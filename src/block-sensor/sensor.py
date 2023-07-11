7f0196b01c6282a6e11efff40d365ec16022adbc
Ivan Sidorov <yvan.sidorov@gmail.com>
2023-07-11 15:50:03 +0800

    flake8 fixes

--- src/block-sensor/sensor.py
+++ src/block-sensor/sensor.py
@@ -17,31 +17,30 @@ logger = pino(
 
 rpc_uri = os.environ.get("RPC_URI")
 queue_name = os.environ.get('RABBITMQ_QUEUE_BLOCK_SENSOR')
-channel=None
+channe = None
+
 
 def connectToRabbitMQ():
     global channel
-
     parameters = pika.URLParameters(os.environ.get('RABBITMQ'))
     try:
         connection = pika.BlockingConnection(parameters)
         channel = connection.channel()
         channel.queue_declare(queue=queue_name, durable=True)
-    except:
+    except Exception:
         logger.error("RabbitMQ connection problem")
 
 
 def sendMessageToRabbitMQ(message: json):
     global channel
-
     if channel is None:
         connectToRabbitMQ()
-    
-    try: 
+
+    try:
         channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(message))
-    except:
+    except Exception:
         connectToRabbitMQ()
-        
+
 
 def fetchLastBlock():
     try:
